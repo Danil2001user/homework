3496bd8119199f0323bbe8a8050b9df18ee9de37
Danil2001user <demon.dani2017@yandex.ru>
2025-11-25 14:14:50 +0400

    Add files via upload

--- Макет сайта 3 с использованием js/js/main (10).js
+++ Макет сайта 3 с использованием js/js/main (10).js
@@ -28,7 +28,63 @@
 
     }
 
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about-img_button')
 
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal-cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
+    // Первый вариант модалки!
+    
+    // const openButton = document.querySelector('.about-img_button');
+    // const modal = document.querySelector('.modal');
+    // const closeButton = document.querySelector('.modal-cancel');
+    
+    // function openModal() {
+    //   modal.classList.add('modal--opened');
+    //   document.body.classList.add('body--opened-modal'); 
+    // }
+    
+    
+    // function closeModal() {
+    //   modal.classList.remove('modal--opened');
+    //   document.body.classList.remove('body--opened-modal');
+    // }
+    
+    // if (openButton) {
+    //   openButton.addEventListener('click', openModal);
+    // }
+    
+    // if (closeButton) {
+    //   closeButton.addEventListener('click', closeModal);
+    // }
+    
+    // modal.addEventListener('click', (e) => {
+    //   if (e.target === modal) {
+    //     closeModal();
+    //   }
+    // });
+    
+    // document.addEventListener('keydown', (e) => {
+    //   if (e.key === 'Escape' && modal.classList.contains('modal--opened')) {
+    //     closeModal();
+    //   }
+    // });
 
 })()
-
