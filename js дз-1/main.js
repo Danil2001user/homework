2bb425dedc037499bdb7164ffde1681fb9626344
Danil2001user <demon.dani2017@yandex.ru>
2026-05-12 22:34:06 +0400

    Add files via upload

---
+++ js дз-1/main.js
@@ -0,0 +1,43 @@
+// Задача-1
+
+// let firstName = "Данил";  
+// let lastName = "Демин";    
+// let isStudent = true;       
+
+// Задача-2
+
+
+// let age = 25;
+
+
+// let currentYear = 2026;
+
+
+// let birthYear = currentYear - age;
+
+// console.log(`Год рождения студента: ${birthYear}`);
+
+
+// Задача-3
+
+// Предположим, что переменные уже объявлены (как в предыдущих задачах)
+let firstName = "Данила";
+let lastName = "Демин";
+let age = 25;
+let isStudent = true;
+
+// Выводим сообщение в консоли в требуемом формате
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`);
+
+// Задача-4
+
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+
+
+// '123456789falsetrue'
+
